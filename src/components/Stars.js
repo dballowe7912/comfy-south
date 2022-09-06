8e3f9c652cafe10908d2546432ee37a3cfcfd490
Donald Ballowe <dballowe7912@gmail.com>
2022-09-05 19:40:14 -0500

    Setup Filters component and functionality

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = ({ stars, reviews }) => {
-  console.log(stars)
- 
+const Stars = ({ stars, reviews }) => { 
   const tempStars = Array.from({ length: 5 }, (_, index) => {
     const number = index + 0.5
     return (
